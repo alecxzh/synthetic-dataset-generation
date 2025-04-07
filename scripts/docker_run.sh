0198309783058cb160e02ae9d000329f3cd434ce
alecxzh <42911602+alecxzh@users.noreply.github.com>
2025-04-06 21:07:01 -0400

    version control + non linux support

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-session_name=parcel2d
-# kill old session if exists
-tmux kill-session -t "${session_name}"
-sleep 1
+# Optional: Stop and remove any existing container named parcel2d
+docker rm -f parcel2d 2>/dev/null
 
-# start new tmux session
-tmux new-session -s "${session_name}" 'docker run -it --rm --name parcel2d \
---mount type=bind,source=${PWD}/data/backgrounds,target=/data/backgrounds \
---mount type=bind,source=${PWD}/data/objects,target=/data/objects \
---mount type=bind,source=${PWD}/data/distractors,target=/data/distractors \
---mount type=bind,source=${PWD},target=/app \
- generate_parcel2d:latest'
+# Run the Docker container directly
+docker run -it --rm --name parcel2d \
+  --mount type=bind,source="${PWD}/data/backgrounds",target=/data/backgrounds \
+  --mount type=bind,source="${PWD}/data/objects",target=/data/objects \
+  --mount type=bind,source="${PWD}/data/distractors",target=/data/distractors \
+  --mount type=bind,source="${PWD}",target=/app \
+  generate_parcel2d:latest
